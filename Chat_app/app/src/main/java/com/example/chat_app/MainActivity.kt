afc416360ca377f70ca099a8c44386558578dbfb
Iago América <iago6489@gmail.com>
2021-10-27 07:49:57 -0300

    add function
    Adicionando uma função no botão

--- Chat_app/app/src/main/java/com/example/chat_app/MainActivity.kt
+++ Chat_app/app/src/main/java/com/example/chat_app/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.chat_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        send_button.setOnClickListener{
+            val message = message_edittext.text.toString()
+            message_edittext.setText("") //setText -> Para colocar o espaço vazio
+            message_textview.text = message //Aqui estamos modificando a propriedade text.
+        }
     }
 }
