cead513955a861b74edb621e2254bce3d1610602
Iago América <iago6489@gmail.com>
2021-10-28 08:28:30 -0300

    Adapter implementation
    Adição do adaptador

--- ListadeCompras_App/app/src/main/java/com/example/lista_de_compras_app/MainActivity.kt
+++ ListadeCompras_App/app/src/main/java/com/example/lista_de_compras_app/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.lista_de_compras_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Implementação do adaptador
+        val produtosAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1)
+
+        //definindo o adaptador na lista
+        list_view_produtos.adapter = produtosAdapter
     }
 }
