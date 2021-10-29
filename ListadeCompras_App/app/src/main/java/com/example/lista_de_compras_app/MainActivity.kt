7863f45b9595ca3164cf192db498aa2a3eb76e04
Iago América <iago6489@gmail.com>
2021-10-29 07:59:21 -0300

    Modification in listview
    Adicionei o metodo remove, para quando o usuário querer remover o item só realizar um click

--- ListadeCompras_App/app/src/main/java/com/example/lista_de_compras_app/MainActivity.kt
+++ ListadeCompras_App/app/src/main/java/com/example/lista_de_compras_app/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.lista_de_compras_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,13 +19,28 @@ class MainActivity : AppCompatActivity() {
         list_view_produtos.adapter = produtosAdapter
 
         //definição do ouvinte do botão
-        btn_inserir.setOnClickListener() {
+        btn_inserir.setOnClickListener {
             val produto = txt_produto.text.toString() //pegando o valor digitado pelo usuário
 
-            produtosAdapter.add(produto) //enviando o item para a lista
+            //produtosAdapter.add(produto) //enviando o item para a lista
+
+            if (produto.isNotEmpty()) {
+                produtosAdapter.add(produto) //enviando o item para a lista
+                txt_produto.text.clear() //limpando a caixa de texto
+            } else {
+                txt_produto.error = "Preencha um valor"
+            }
         }
 
+        list_view_produtos.setOnItemLongClickListener{ adapterView: AdapterView<*>,
+                                                       view: View, position: Int, id: Long ->
 
+            val	item = produtosAdapter.getItem(position) //buscando o item clicado
 
+            produtosAdapter.remove(item) // removendo o item clicado da lista
+            
+            true //retorno indicando que o click foi realizado com sucesso
+
+        }
     }
 }
