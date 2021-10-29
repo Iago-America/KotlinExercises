89e8c79b49ffca9e4edb1dc0879ca03ae39868f5
Iago América <iago6489@gmail.com>
2021-10-29 11:42:18 -0300

    edit import

--- ListadeCompras_App/app/src/main/java/com/example/lista_de_compras_app/CadastroActivity.kt
+++ ListadeCompras_App/app/src/main/java/com/example/lista_de_compras_app/CadastroActivity.kt
@@ -2,6 +2,7 @@ package com.example.lista_de_compras_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import	kotlinx.android.synthetic.main.activity_cadastro.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +10,15 @@ class CadastroActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cadastro)
 
         //definição do ouvinte do botão
+        btn_inserir.setOnClickListener {
+            val produto = txt_produto.text.toString() //pegando o valor digitado pelo usuário
 
+            if (produto.isNotEmpty()) {
+                //enviando o item para a lista
+                txt_produto.text.clear() //limpando a caixa de texto
+            } else {
+                txt_produto.error = "Preencha um valor"
+            }
+        }
     }
 }
