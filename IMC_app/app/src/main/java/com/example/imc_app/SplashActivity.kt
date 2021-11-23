3a3f26efd158ebeefe4b31c937d9dc752c67ec16
Iago América <iago6489@gmail.com>
2021-11-23 16:00:31 -0300

    SplashActivity

--- IMC_app/app/src/main/java/com/example/imc_app/SplashActivity.kt
+++ IMC_app/app/src/main/java/com/example/imc_app/SplashActivity.kt
@@ -1,4 +1,23 @@
 package com.example.imc_app
 
-class SplashActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splashactivity)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Chama a LoginActivity
+            val mIntent= Intent(this,MainActivity::class.java)
+            //método responsável por executar a Intent
+            startActivity(mIntent)
+            //método responsável por remover a tela da pilha
+            finish()
+        },3000)//tempo em milisegundos
+    }
 }
